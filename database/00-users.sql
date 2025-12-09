954f8a1ff1b49ea1ef70db03262ece2d236679e6
Dom <157792207+lunarjuun@users.noreply.github.com>
2025-12-09 09:38:03 +0100

    Small changes to database

--- database/00-users.sql
+++ database/00-users.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS users
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(50),
     username VARCHAR(50) NOT NULL UNIQUE,
-    password_hash TEXT NOT NULL,
+    password_hash TEXT(50) NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     last_login TIMESTAMPTZ,
 )
